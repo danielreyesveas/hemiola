c92d24a09122f70f9d9b2449367f071f1f42d245
Daniel Reyes Veas <danielreyesveas@gmail.com>
2021-03-08 21:37:47 +0100

    API_KEY

--- core/constants.py
+++ core/constants.py
@@ -3,3 +3,4 @@ from django.conf import settings
 contact_template_slug =  'hemiola_contact'
 newsletter_template_slug =  'hemiola_newsletter'
 email_sender_url = settings.EMAIL_SENDER_URL
+email_sender_api_key = settings.EMAIL_SENDER_API_KEY
